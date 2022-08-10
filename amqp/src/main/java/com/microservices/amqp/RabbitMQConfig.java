5a9de2611ea8b8f3fc9434e412e7c6f3a4a990e0
plebedev <plebedev@alfabank.ru>
2022-08-10 17:06:04 +0400

    AMQP module connected with producer for RabbitMQ

---
+++ amqp/src/main/java/com/microservices/amqp/RabbitMQConfig.java
@@ -0,0 +1,40 @@
+package com.microservices.amqp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setMessageConverter(messageConverter());
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        return rabbitTemplate;
+    }
+
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setMessageConverter(messageConverter());
+        factory.setConnectionFactory(connectionFactory);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MessageConverter messageConverter = new Jackson2JsonMessageConverter();
+        return messageConverter;
+    }
+}
--- notification/src/main/java/com/microservices/notification/NotificationApplication.java
+++ notification/src/main/java/com/microservices/notification/NotificationApplication.java
@@ -1,16 +1,34 @@
 package com.microservices.notification;
 
+import com.microservices.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.microservices.notification",
+                "com.microservices.amqp",
+        }
+)
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(
+//                    "foo",
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
 }
